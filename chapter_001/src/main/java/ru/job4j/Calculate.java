3887faa267382c8955c95ff2d5e8fc85b40c806a
AlexanderZf44 <rejmond.a@yandex.ru>
2017-10-09 18:55:18 +0500

    Task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+*Class Calculate для вывода текста на кансоль
+*@author apermyakov
+*@since 09.10.2017
+*@version 1
+*/
+
 public class Calculate {
+	
+	/**
+	*Вывод строки на консоль
+	*/
+	
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+*Package foe calculate task.
+*
+*@author apermyakov
+*@version $Id$
+*@since 0.1
+*/
+package ru.job4j;
