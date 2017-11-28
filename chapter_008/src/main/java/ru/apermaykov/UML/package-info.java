5f6c38d1639a29d83a6542d73a562c8095f20caf
AlexanderZf44 <rejmond.a@yandex.ru>
2017-11-28 08:34:41 +0500

    Task 1093 SQL Scripts

---
+++ chapter_008/src/main/java/ru/apermaykov/UML/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for sql scripts for uml system.
+ *
+ * @author apermyakov
+ * @version 1.0
+ * @since 28.11.2017
+ */
+package ru.apermaykov.UML;
---
+++ chapter_008/src/main/java/ru/apermaykov/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for chapter_008.
+ *
+ * @author apermyakov
+ * @version 1.0
+ * @since 24.11.2017
+ */
+package ru.apermaykov;
