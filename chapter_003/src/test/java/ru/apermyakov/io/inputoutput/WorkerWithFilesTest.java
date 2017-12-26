8fc126fb8f16dc627f5210a599daa28b0203d5a2
AlexanderZf44 <rejmond.a@yandex.ru>
2017-12-26 16:39:33 +0500

    Task 860 Update
    Task 860 Update

--- chapter_003/src/test/java/ru/apermyakov/io/inputoutput/WorkerWithFilesTest.java
+++ chapter_003/src/test/java/ru/apermyakov/io/inputoutput/WorkerWithFilesTest.java
@@ -3,6 +3,12 @@ package ru.apermyakov.io.inputoutput;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
 
 /**
  * Class for test worker with files.
@@ -17,10 +23,24 @@ public class WorkerWithFilesTest {
      * When sort two files then build two temp files and sort to result file.
      */
     @Test
-    public void whenSortTwoFilesThenBuildTwoTempFilesAndSortToResultFile() {
+    public void whenSortTwoFilesThenBuildTwoTempFilesAndSortToResultFile() throws IOException {
         WorkerWithFiles worker = new WorkerWithFiles();
         File source = new File("C:\\check\\source.txt");
         File distance = new File("C:\\check\\distance.txt");
+        String fileLine;
+        String tempLine ="";
+        boolean sorted = true;
+
         worker.sort(source, distance);
+        try (RandomAccessFile file = new RandomAccessFile(distance, "r")) {
+            while ((fileLine = file.readLine()) != null) {
+                if (tempLine.length() > fileLine.length()) {
+                    sorted = false;
+                }
+                tempLine = fileLine;
+            }
+        }
+
+        assertThat(sorted, is(true));
     }
 }
