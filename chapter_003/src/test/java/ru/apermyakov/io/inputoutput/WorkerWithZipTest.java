1bd0e479e988f63006fa03d3fa7b3ea40e60bd9d
AlexanderZf44 <rejmond.a@yandex.ru>
2017-12-26 16:56:31 +0500

    Task 861 Update

--- chapter_003/src/test/java/ru/apermyakov/io/inputoutput/WorkerWithZipTest.java
+++ chapter_003/src/test/java/ru/apermyakov/io/inputoutput/WorkerWithZipTest.java
@@ -2,7 +2,13 @@ package ru.apermyakov.io.inputoutput;
 
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.zip.ZipInputStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 
 /**
  * Class for test zip worker.
@@ -17,11 +23,19 @@ public class WorkerWithZipTest {
      * Test when choose direction and file's keys then build archive.
      */
     @Test
-    public void whenChooseDirectionAndFilesKeysThenBuildArchive() {
+    public void whenChooseDirectionAndFilesKeysThenBuildArchive() throws IOException {
         WorkerWithZip worker = new WorkerWithZip();
         ArrayList<String> keys = new ArrayList<>();
         keys.add(".txt");
         keys.add(".java");
+        String result;
+
         worker.convertToZip("C:\\check", keys);
+        try (ZipInputStream zip = new ZipInputStream(new FileInputStream("C:\\Projects\\APermyakov\\chapter_003\\archive.zip"))) {
+            result = zip.getNextEntry().getName();
+
+        }
+
+        assertThat(result.contains("C:\\check\\chapter_009"), is(true));
     }
 }
