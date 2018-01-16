db806619b3c3d067460f89128da7dac1b17d9a68
AlexanderZf44 <rejmond.a@yandex.ru>
2018-01-16 10:41:51 +0500

    Task 4744

---
+++ chapter_010/src/main/java/ru/apermyakov/mapping/Engine.java
@@ -0,0 +1,54 @@
+package ru.apermyakov.mapping;
+
+public class Engine {
+
+    private int id;
+
+    private String name;
+
+    private String type;
+
+    private int cylinders;
+
+    private double horsepower;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+
+    public double getHorsepower() {
+        return horsepower;
+    }
+
+    public void setHorsepower(double horsepower) {
+        this.horsepower = horsepower;
+    }
+}
