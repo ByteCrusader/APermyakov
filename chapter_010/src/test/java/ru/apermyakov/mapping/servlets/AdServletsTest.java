8e5e6ffba56073d82d60946745988d2c164d8e98
AlexanderZf44 <rejmond.a@yandex.ru>
2018-01-24 15:06:28 +0500

    Task 4746 Update

--- chapter_010/src/test/java/ru/apermyakov/mapping/servlets/AdServletsTest.java
+++ chapter_010/src/test/java/ru/apermyakov/mapping/servlets/AdServletsTest.java
@@ -2,21 +2,17 @@ package ru.apermyakov.mapping.servlets;
 
 import org.junit.Test;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Base64;
-import java.util.Enumeration;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -31,92 +27,7 @@ public class AdServletsTest {
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
 
-        HttpSession session = new HttpSession() {
-            @Override
-            public long getCreationTime() {
-                return 0;
-            }
-
-            @Override
-            public String getId() {
-                return null;
-            }
-
-            @Override
-            public long getLastAccessedTime() {
-                return 0;
-            }
-
-            @Override
-            public ServletContext getServletContext() {
-                return null;
-            }
-
-            @Override
-            public void setMaxInactiveInterval(int i) {
-
-            }
-
-            @Override
-            public int getMaxInactiveInterval() {
-                return 0;
-            }
-
-            @Override
-            public HttpSessionContext getSessionContext() {
-                return null;
-            }
-
-            @Override
-            public Object getAttribute(String s) {
-                return "Ivan";
-            }
-
-            @Override
-            public Object getValue(String s) {
-                return null;
-            }
-
-            @Override
-            public Enumeration<String> getAttributeNames() {
-                return null;
-            }
-
-            @Override
-            public String[] getValueNames() {
-                return new String[0];
-            }
-
-            @Override
-            public void setAttribute(String s, Object o) {
-
-            }
-
-            @Override
-            public void putValue(String s, Object o) {
-
-            }
-
-            @Override
-            public void removeAttribute(String s) {
-
-            }
-
-            @Override
-            public void removeValue(String s) {
-
-            }
-
-            @Override
-            public void invalidate() {
-
-            }
-
-            @Override
-            public boolean isNew() {
-                return false;
-            }
-        };
+        HttpSession session = mock(HttpSession.class);
 
         when(request.getHeader("Authorization")).thenReturn("Basic " + new String(Base64.getEncoder().encode("check@mail.ru : ***".getBytes())));
         when(request.getSession()).thenReturn(session);
@@ -129,6 +40,7 @@ public class AdServletsTest {
         when(request.getParameter("engineId")).thenReturn("1");
         when(request.getParameter("gearboxId")).thenReturn("1");
         when(request.getParameter("transmissionId")).thenReturn("1");
+        when(session.getAttribute("Ivan")).thenReturn("Ivan");
 
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
